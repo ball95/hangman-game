8bafcc992deb07db646ca3825212e9cf9e2e8d32
ball <cosmopolitanism903@gmail.com>
2024-02-23 19:14:05 +1100

    updated win and lose screen

--- game.cpp
+++ game.cpp
@@ -18,24 +18,46 @@ int main()
 
 	std::wstring displayedWord(word.size(), '*');
 
-	while (lives > 0) {
+	while (true) {
+
+		static bool correct; // static bool to check if input was correct or not
+		static wchar_t input;
+		correct = false;
+
+
 		system("cls");
 		std::wcout << " WORD: " << displayedWord << '\n';
 		std::wcout << "LIVES: " << lives << '\n';
 
-		static bool correct; // static bool to check if input was correct or not
-		correct = false;
+		// check if win
+		if (displayedWord == word) {
+			std::wcout << "Fuggoi you winned" << '\n';
+			std::cin.get();
+			break;
+		}
 
-		static wchar_t input; 
+		// check if lives are 0
+		if (lives <= 0) {
+			std::wcout << "You Lose" << '\n';
+			std::wcout << "Word: " << word << '\n';
+			std::cin.get();
+			break;
+		}
+
+		// if lives are above 0 ask for input
 		std::wcout << "Guess: ";
 		std::wcin >> input;
 
+		// loop through every letter in original word
 		for (int i = 0; i < word.size(); ++i) {
 			if (input == word[i]) {
 				correct = true;
 				displayedWord[i] = word[i];
 			}
 		}
+
+		// clear istream so program doesn't loop again with the next characters in istream if the user input more than one character aye?
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
 		if (!correct) {
 			--lives; // if input did not match any of the letters take away a life
